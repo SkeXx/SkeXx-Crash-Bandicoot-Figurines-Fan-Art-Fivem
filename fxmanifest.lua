0136868965cac33c4896f8586bc79b6bb7236206
SkeXx <gaudjulien855@gmail.com>
2026-06-09 16:13:54 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'SkeXx Crash Bandicoot Fan Art'
+
+files {
+    'stream/**/*.ybn',
+    'stream/**/*.ydr',
+    'stream/**/*.ytd',
+    'stream/**/*.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'

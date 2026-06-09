74634661e4115ec370d30a04f892308139c2e626
SkeXx <gaudjulien855@gmail.com>
2026-06-09 16:27:42 +0200

    Add files via upload

---
+++ Attach Props.lua
@@ -0,0 +1,119 @@
+local FIGURINES_CARRY = {
+    tawna_bandicoot = {
+        model  = "skexx_tawna_bandicoot",
+        offset = vec3(-0.0006, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    uka_uka = {
+        model  = "skexx_uka_uka",
+        offset = vec3( 0.0005, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    aku_aku = {
+        model  = "skexx_aku_aku",
+        offset = vec3(-0.0028, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    alt_tawna = {
+        model  = "skexx_Alt_Tawna",
+        offset = vec3( 0.0043, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    coco_bandicoot = {
+        model  = "skexx_coco_bandicoot",
+        offset = vec3(-0.0018, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    crash_bandicoot = {
+        model  = "skexx_crash_bandicoot",
+        offset = vec3( 0.0012, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    crunch_bandicoot = {
+        model  = "skexx_crunch_bandicoot",
+        offset = vec3(-0.0010, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    dr_n_gin = {
+        model  = "skexx_dr_n_gin",
+        offset = vec3(-0.0010, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    neo_cortex = {
+        model  = "skexx_neo_cortex",
+        offset = vec3(-0.0091, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    papu_papu = {
+        model  = "skexx_papu_papu",
+        offset = vec3(-0.0007, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    pinstripe_potoroo = {
+        model  = "skexx_pinstripe_potoroo",
+        offset = vec3( 0.0050, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    ripper_roo = {
+        model  = "skexx_ripper_roo",
+        offset = vec3(-0.0057, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    tiny_tiger = {
+        model  = "skexx_tiny_tiger",
+        offset = vec3( 0.0005, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    dingodile = {
+        model  = "skexx_dingodile",
+        offset = vec3( 0.0041, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    dr_nefarious_tropy = {
+        model  = "skexx_dr_nefarious_tropy",
+        offset = vec3(-0.0003, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    koala_kong = {
+        model  = "skexx_koala_kong",
+        offset = vec3(-0.0037, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    komodo_joe = {
+        model  = "skexx_komodo_joe",
+        offset = vec3( 0.0022, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    komodo_moe = {
+        model  = "skexx_komodo_moe",
+        offset = vec3(-0.0035, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+
+    n_tropy_female = {
+        model  = "skexx_n_tropy_female",
+        offset = vec3( 0.0009, 0.0860, -0.1606),
+        rot    = vec3(90.0, 0.0, 180.0)
+    },
+}
+
+local HAND_BONE = 57005
+
+
